e62dfa1fe562140db5d8aea82d298c07681d0c5c
Luan Evander <luanevander.dev@gmail.com>
2024-09-07 20:16:58 -0300

    refactor(PerguntaTres)remove importações desnecessárias

--- src/com/desafiotarget/perguntatres/PerguntaTres.java
+++ src/com/desafiotarget/perguntatres/PerguntaTres.java
@@ -1,10 +1,5 @@
 package com.desafiotarget.perguntatres;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PerguntaTres {
