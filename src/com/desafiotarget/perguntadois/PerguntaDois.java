109f43eadc93ecf6465662d257327e9912eafda2
Luan Evander <luanevander.dev@gmail.com>
2024-09-07 19:00:16 -0300

    feat(Pergunta Dois): Adiciona verificação se um número pertence à sequência de Fibonacci

--- src/com/desafiotarget/perguntadois/PerguntaDois.java
+++ src/com/desafiotarget/perguntadois/PerguntaDois.java
@@ -1,5 +1,43 @@
 package com.desafiotarget.perguntadois;
 
+import java.util.Scanner;
+
 public class PerguntaDois {
-    
+    public static boolean pertenceAoFibonacci(int num) {
+        if (num < 0) {
+            return false;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        if (num == a || num == b) {
+            return true;
+        }
+
+        while (b <= num) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+
+            if (b == num) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("\nDigite um número inteiro: ");
+        int num = scanner.nextInt();
+
+        if (pertenceAoFibonacci(num)) {
+            System.out.println("O número " + num + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + num + " não pertence à sequência de Fibonacci.");
+        }
+    }
 }
